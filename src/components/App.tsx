6ecd1421cc11285d43458e9811bcff01b918b0be
David De Vargas <dvccueter@gmail.com>
2021-10-16 20:23:22 -0500

    style: adding styles to button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,25 @@
 import './App.sass'
 import { Button } from './atoms/Button'
 
+import thumb_up from '../assets/thumb_up.svg'
+import thumb_down from '../assets/thumb_down.svg'
+
 function App() {
 
   return (
-    <>
-      <Button onClick={() => console.log('click')} className='primary'>
-        Some text
+    <div style={{background: 'black'}}>
+      <Button onClick={() => console.log('click')} className="primary selected" label="thumb up button">
+        <img src={thumb_up} alt="thumb up logo" />
       </Button>
-    </>
+
+      <Button onClick={() => console.log('click')} className="secondary" label="thumb down button">
+        <img src={thumb_down} alt="thumb down logo" />
+      </Button>
+
+      <Button onClick={() => console.log('click')} label="Secondary button" type="submit">
+        Vote now
+      </Button>
+    </div>
   )
 }
 
