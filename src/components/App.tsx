dd126d1e91db7753d4582a0c27aca9e21b2599e8
David De Vargas <dvccueter@gmail.com>
2021-10-18 14:00:23 -0500

    test: simple Card test case

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,10 +5,21 @@ import { Card } from './organisms'
 
 
 function App() {
-
+  const cardData = {
+      name: "Kanye West",
+      description: "Born in Atlanta and raised in Chicago, West was first known as a producer for Roc-A-Fella Records in the early 2000s, producing singles for several mainstream artists.",
+      category: "entertainment",
+      picture: "kanye.png",
+      lastUpdated: "2020-03-10T23:08:57.892Z",
+      votes: {
+          positive: 23,
+          negative: 36
+      }
+  }
   return (
     <div style={{background: 'lightgray'}}>
-      <Card />
+      <Card {...cardData} />
+
       <VoteForm
         onSubmit={() => console.log('submit')}
         className=""
--- src/components/organisms/Card/card.props.ts
+++ src/components/organisms/Card/card.props.ts
@@ -1,1 +1,8 @@
-export default interface Props {}
+export default interface Props {
+  name: string;
+  description: string;
+  category: string;
+  picture: string;
+  lastUpdated: string;
+  votes: { positive: number; negative: number; }
+}
