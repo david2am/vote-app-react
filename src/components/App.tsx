ee99ef72d8247550b5647d589c94aa2e22dd8a6e
David De Vargas <dvccueter@gmail.com>
2021-10-18 23:34:26 -0500

    feat: main component builded

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,11 @@
 import './App.sass'
 import { Main } from './organisms'
-import { useQuery } from 'urql'
-import { CharactersQuery } from '../graphql'
-
 
 function App() {
-  const [result] = useQuery({ query: CharactersQuery })
-
-  const { data, fetching, error } = result
-
-  if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oh no... {error.message}</p>;
-
   return (
-    <pre>
-      {JSON.stringify(data, null, 2)}
-    </pre>
+    <>
+      <Main />
+    </>
   )
 }
 
--- src/components/organisms/Main/Main.tsx
+++ src/components/organisms/Main/Main.tsx
@@ -1,8 +1,27 @@
 import './_main.sass'
+import CardProps from '../Card/card.props'
+import { Card } from '../../organisms'
+
+import { useQuery } from 'urql'
+import { CharactersQuery } from '../../../graphql'
+
 
 const Main = () => {
+  const [result] = useQuery({ query: CharactersQuery })
+
+  const { data, fetching, error } = result
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <h2>Oh no... error {error.message}</h2>;
+
   return (
-    <></>
+    <ul role="menubar" aria-label="list of characters to vote">
+      {data.data.map((character: CardProps) => (
+        <li role="none" key={character.id}>
+          <Card {...character} />
+        </li>
+      ))}
+    </ul>
   )
 }
 
