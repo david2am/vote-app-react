b3d137c27e84a86137282130527041c9be191b74
David De Vargas <dvccueter@gmail.com>
2021-10-20 12:10:22 -0500

    feat: select element created

--- src/components/atoms/Select/Select.tsx
+++ src/components/atoms/Select/Select.tsx
@@ -3,11 +3,19 @@ import Props from './select.props'
 
 const Select = (
   {
-    optionList
+    optionList,
+    label
   }: Props
 ) => {
   return (
-    <></>
+    <select
+      role="select"
+      aria-label={label}
+    >
+      {optionList.map(({ id, value }) => (
+        <option value={value} key={id} >{value}</option>
+      ))}
+    </select>
   )
 }
 
--- src/components/organisms/Main/Main.tsx
+++ src/components/organisms/Main/Main.tsx
@@ -1,6 +1,7 @@
 import './_main.sass'
 import CardProps from '../Card/card.props'
 import { Card } from '../../organisms'
+import { Select } from '../../atoms'
 
 import { useQuery } from 'urql'
 import { CharactersQuery } from '../../../graphql'
@@ -8,6 +9,7 @@ import { CharactersQuery } from '../../../graphql'
 import { useContext } from 'react'
 import { CharacterContext } from '../../../context/CharacterProvider'
 
+const list = [ { id: 1, value: 'List'}, { id: 2, value: 'Grid' } ] // TODO: it should be fetched
 
 const Main = () => {
   const { characterList, setCharacterList } = useContext(CharacterContext)
@@ -19,21 +21,24 @@ const Main = () => {
   setCharacterList(data.data)
 
   return (
-    <ul
-      role="menubar"
-      className="cardList"
-      aria-label="list of characters to vote"
-    >
-      {characterList.map((character: CardProps) => (
-        <li
-          role="none"
-          key={character.id}
-          className="cardList__item"
-        >
-          <Card {...character} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <Select label="Select between grid and list view" optionList={list} />
+      <ul
+        role="menubar"
+        className="cardList"
+        aria-label="list of characters to vote"
+      >
+        {characterList.map((character: CardProps) => (
+          <li
+            role="none"
+            key={character.id}
+            className="cardList__item"
+          >
+            <Card {...character} />
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
