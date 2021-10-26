91c50059849b31bae53f75135d27b3290db479dc
David De Vargas <dvccueter@gmail.com>
2021-10-26 08:31:54 -0500

    style: hidding selector

--- src/components/organisms/Main/Main.tsx
+++ src/components/organisms/Main/Main.tsx
@@ -9,24 +9,20 @@ import { CHARACTER_QUERY } from '../../../graphql'
 import { ChangeEvent, useContext, useEffect, useState } from 'react'
 import { CharacterContext } from '../../../context/CharacterProvider'
 
-const list = [ { id: 1, value: 'List'}, { id: 2, value: 'Grid' } ] // TODO: it should be fetched
+const list = [ { id: 1, value: 'Grid'}, { id: 2, value: 'List' } ] // TODO: it should be fetched
 
-function isGrid (className: string, isSelectVisible: boolean, selectValue: string): string {
-  return isSelectVisible && selectValue === 'Grid' ? `${className}-grid` : `${className}-list`
+function getClassAndModifier (className: string, selectValue: string): string {
+  const classModifier = selectValue === 'List' ? `${className}-list` : `${className}-grid`
+  return `${className} ${classModifier}`
 }
 
 const Main = () => {
   // state
-  const [isSelectVisible, setIsSelectVisible] = useState<boolean>(true)
   const [selectValue, setSelectValue] = useState<string>('')
 
   // effects
   const { characterList, setCharacterList } = useContext(CharacterContext)
   const [{ data, fetching, error }] = useQuery({ query: CHARACTER_QUERY })
-
-  useEffect(() =>
-    setIsSelectVisible(document.documentElement.clientWidth > 768)
-  , [])
 
   useEffect(() =>
     data && setCharacterList(data.allCharacters)
@@ -46,25 +42,23 @@ const Main = () => {
         <h2 className="main__header__title">
           Previous Rulings
         </h2>
-        { isSelectVisible &&
-          <Select
-            label="Select between grid and list view"
+        <Select
+            label="Select between grid or list view"
             className="main__header__selector"
             onChange={handleSelect}
             optionList={list}
           />
-        }
       </div>
       <ul
         role="menubar"
-        className={`main__cardList ${ isGrid('main__cardList', isSelectVisible, selectValue) }`}
+        className={getClassAndModifier('main__cardList', selectValue)}
         aria-label="list of characters to vote"
       >
         {(characterList).map((character: CardProps) => (
           <li
             role="none"
             key={character.id}
-            className={`main__cardList__item ${isGrid('main__cardList__item', isSelectVisible, selectValue)}`}
+            className={getClassAndModifier('main__cardList__item', selectValue)}
           >
             <Card {...character} />
           </li>
