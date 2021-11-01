d2fe0e28a8fbb88b17d50c33c06b430b4114e441
David De Vargas <dvccueter@gmail.com>
2021-10-31 19:59:46 -0500

    style (Info): &--list @media

--- src/components/molecules/Info/Info.tsx
+++ src/components/molecules/Info/Info.tsx
@@ -1,13 +1,17 @@
 import './_info.sass'
 import Props from './info.props'
+import { useContext } from 'react'
+import { ViewContext } from '../../../context'
 
 const Info = ({
   name,
   description,
   className
 }: Props) => {
+  const { getViewModifier } = useContext(ViewContext)
+
   return (
-    <section className={`info ${className}`}>
+    <section className={`info ${className} ${getViewModifier('info')}`}>
 
       <h2 className="info__title"> {name} </h2>
 
