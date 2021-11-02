85d74de90da208b8ad389e0f630265c8d13e177b
David De Vargas <dvccueter@gmail.com>
2021-11-02 00:13:07 -0500

    style (FeedbackForm): adding responsiveness

--- src/components/organisms/FeedbackForm/FeedbackForm.tsx
+++ src/components/organisms/FeedbackForm/FeedbackForm.tsx
@@ -5,33 +5,34 @@ import './_feedbackForm.sass'
 import background from '../../../assets/background.png'
 
 const FeedbackForm = () => {
+
+  // handlers
   const handleSubmit = (event: FormEvent): void => {
     event.preventDefault()
   }
 
   return (
     <form
-      // className={`feedForm`}
+      className={`feedForm`}
       onSubmit={handleSubmit}
     >
-      
-      <div className="feedForm">
-        <img
-          className="feedForm__img"
-          src={background} alt="background image"
-        />
 
-        <p className="feedForm__paragraph">Is there anyone else you would want us to add?</p>
+      <img
+        className="feedForm__img"
+        src={background} alt="background image"
+      />
+
+      <div className="feedForm__content">
+        <p className="feedForm__content__paragraph">Is there anyone else you would want us to add?</p>
 
         <Button
-          className="feedForm__btn btn__feedback"
+          className="btn__feedback feedForm__content__btn"
           label="Submit feedback"
           type="submit"
         >
           Submit a name
         </Button>
       </div>
-
       
     </form>
   )
