89c483f7be2ab332cd5f99e6e87fcc0c429962bc
David De Vargas <dvccueter@gmail.com>
2021-11-01 22:53:21 -0500

    style (FeedbackForm): adding some style

--- src/components/organisms/FeedbackForm/FeedbackForm.tsx
+++ src/components/organisms/FeedbackForm/FeedbackForm.tsx
@@ -1,5 +1,8 @@
 import { FormEvent } from 'react'
+import { Button } from '../../atoms'
 import './_feedbackForm.sass'
+
+import background from '../../../assets/background.png'
 
 const FeedbackForm = () => {
   const handleSubmit = (event: FormEvent): void => {
@@ -7,12 +10,28 @@ const FeedbackForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <img src="" alt="background image" />
+    <form
+      className={`feedForm`}
+      onSubmit={handleSubmit}
+    >
+      <img
+        className="feedForm__img"
+        src={background} alt="background image"
+      />
 
-      <p>Is there anyone else you would want us to add?</p>
+      <div>
+        <p className="feedForm__paragraph">Is there anyone else you would want us to add?</p>
 
-      <button type="submit">Submit a name</button>
+        <Button
+          className="feedForm__btn btn__feedback"
+          label="Submit feedback"
+          type="submit"
+        >
+          Submit a name
+        </Button>
+      </div>
+
+      
     </form>
   )
 }
